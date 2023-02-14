e25d216974e9d4d9a86862149d9d2bc40d66e3ce
Donn Neufeld <donn.neufeld@gmail.com>
2023-02-13 17:15:44 -0800

    refactor subclass-dance-party classes from funcfrom functional inheritance to pseudoclassical.
    bug with toggle to needs fix

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,34 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  //old - var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  MakeDancer.call(this);
+  this.setPosition(top, left);
 
+
+};
+MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
+MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
+MakeBlinkyDancer.prototype.oldStep = function () {
+  setTimeout(this.oldStep, this.timeBetweenSteps);;
 };
+
+MakeBlinkyDancer.prototype.step = function () {
+  this.oldStep(this.$node.toggle());
+
+};
+  //var oldStep = blinkyDancer.step;
+
+  //OLD VERSION (PROVIDED VERSION) of
+  // blinkyDancer.step = function() {
+  //   // call the old version of step at the beginning of any call to this new version of step
+  //   oldStep();
+  //   // toggle() is a jQuery method to show/hide the <span> tag.
+  //   // See http://api.jquery.com/category/effects/ for this and
+  //   // other effects you can use on a jQuery-wrapped html tag.
+  //   blinkyDancer.$node.toggle();
+  // };
+
+//   return blinkyDancer;
+// };
